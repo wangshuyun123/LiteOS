a439b21abc06850b106f9b8c78861afed457fe25
wangshuyun123 <wang_shuyun@hoperun.com>
2018-07-19 20:09:24 +0800

    rename target LPCXpresso51U68 to Standard_LPCXpresso51U68

---
+++ targets/Standard_LPCXpresso51U68/Src/usart.c
@@ -0,0 +1,83 @@
+/*----------------------------------------------------------------------------
+ * Copyright (c) <2016-2018>, <Huawei Technologies Co., Ltd>
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice, this list of
+ * conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list
+ * of conditions and the following disclaimer in the documentation and/or other materials
+ * provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used
+ * to endorse or promote products derived from this software without specific prior written
+ * permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *---------------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------
+ * Notice of Export Control Law
+ * ===============================================
+ * Huawei LiteOS may be subject to applicable export control laws and regulations, which might
+ * include those applicable to Huawei LiteOS of U.S. and the country in which you are located.
+ * Import, export and usage of Huawei LiteOS in any manner by you shall be in compliance with such
+ * applicable export control laws and regulations.
+ *---------------------------------------------------------------------------*/
+ 
+#include "stdlib.h"
+#include "string.h"
+#include <stdio.h>
+#include "usart.h"
+
+
+void LOS_UartInit(void)
+{
+    usart_config_t config;
+    
+    /* attach 12 MHz clock to FLEXCOMM0 (debug console) */
+    CLOCK_AttachClk(BOARD_DEBUG_UART_CLK_ATTACH);
+
+    /* reset FLEXCOMM for USART */
+    RESET_PeripheralReset(kFC0_RST_SHIFT_RSTn);
+    
+    BOARD_InitPins();
+    /*
+     * config.baudRate_Bps = 115200U;
+     * config.parityMode = kUSART_ParityDisabled;
+     * config.stopBitCount = kUSART_OneStopBit;
+     * config.loopback = false;
+     * config.enableTx = false;
+     * config.enableRx = false;
+     */
+    USART_GetDefaultConfig(&config);
+    config.baudRate_Bps = BOARD_DEBUG_UART_BAUDRATE;
+    config.enableTx = true;
+    config.enableRx = true;
+
+    USART_Init(DEMO_USART, &config, DEMO_USART_CLK_FREQ);
+}
+
+/* define fputc */
+#if defined ( __CC_ARM ) || defined ( __ICCARM__ )  /* KEIL and IAR: printf will call fputc to print */
+int fputc(int ch, FILE *f)
+{
+    USART_WriteBlocking(DEMO_USART, (uint8_t *)&ch, 1);
+    return ch;
+}
+#elif defined ( __GNUC__ )  /* GCC: printf will call _write to print */
+__attribute__((used)) int _write(int fd, char *ptr, int len)
+{
+    USART_WriteBlocking(DEMO_USART, (uint8_t *)ptr, len);
+    return len;
+}
+#endif
+
+
